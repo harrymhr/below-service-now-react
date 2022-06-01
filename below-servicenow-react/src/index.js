df7f830ee28776f590074c1a96306290bc0e0853
Harish Singh Mehra <harish.mehra@telusinternational.com>
2022-06-01 17:33:39 +0530

    chj

--- below-servicenow-react/src/index.js
+++ below-servicenow-react/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import App from './components/app/App.js'
+import store from './store/index.js';
+import { customPageData } from './utils/customPageData'
+import './index.scss';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+render(
+    <Provider store={store}>
+        <App customPageData={customPageData}/>
+    </Provider>, document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
